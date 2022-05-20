8fd51e431b3ca278b047eaf354160fed3ce76610
ashikrnhq04 <ashikrn.hq04@gmail.com>
2022-05-20 12:55:56 +0600

    Minor code refactored

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import { useEffect, useState } from "react";
 import ResponsiveAppBar from "./components/Header";
-import Container from "@mui/material/Container";
-import Posts from "./components/Posts";
-import { PaginationRounded } from "./utils/index";
-import Button from "@mui/material/Button";
 import AllPosts from "./views/blog";
 import Notfound from "./views/404";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
